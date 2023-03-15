6cdbc54283bfa8a9616a28204fcafa1448076418
Dana Aljamal <danaaljamal94@gmail.com>
2023-03-15 19:33:59 -0400

    initial commit

---
+++ StudentDBAppnew/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentDBAppnew;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ StudentDBAppnew/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentDBAppnew;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
